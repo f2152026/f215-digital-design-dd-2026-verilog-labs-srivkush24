c41cee368472ec4db9bc5d40fc355c24db376b6f
srivkush24 <f20250757@pilani.bits-pilani.ac.in>
2026-08-29 11:44:12 +0000

    lab01 task4

--- labs/lab01/task4/cla4.v
+++ labs/lab01/task4/cla4.v
@@ -34,15 +34,13 @@ module cla4(
   wire c1, c2, c3;
 
   // TODO: your gate-level P/G, carry, and sum logic goes here.
-  // (cout should be connected to c4.) Remember the delay on every gate.
+  
 wire t_c1_1;
   wire t_c2_1, t_c2_2;
   wire t_c3_1, t_c3_2, t_c3_3;
   wire t_c4_1, t_c4_2, t_c4_3, t_c4_4;
 
-  //----------------------------------------------------------------------------
-  // Step 1: Bitwise Propagate (P) and Generate (G) logic
-  //----------------------------------------------------------------------------
+ 
   xor #(2) (p0, a[0], b[0]);
   xor #(2) (p1, a[1], b[1]);
   xor #(2) (p2, a[2], b[2]);
@@ -53,34 +51,29 @@ wire t_c1_1;
   and #(2) (g2, a[2], b[2]);
   and #(2) (g3, a[3], b[3]);
 
-  //----------------------------------------------------------------------------
-  // Step 2: Carry Lookahead Logic Equations
-  //----------------------------------------------------------------------------
-  // c1 = g0 + p0*cin
+  
   and #(2) (t_c1_1, p0, cin);
   or  #(2) (c1, g0, t_c1_1);
 
-  // c2 = g1 + p1*g0 + p1*p0*cin
+  
   and #(2) (t_c2_1, p1, g0);
   and #(2) (t_c2_2, p1, p0, cin);
   or  #(2) (c2, g1, t_c2_1, t_c2_2);
 
-  // c3 = g2 + p2*g1 + p2*p1*g0 + p2*p1*p0*cin
+  
   and #(2) (t_c3_1, p2, g1);
   and #(2) (t_c3_2, p2, p1, g0);
   and #(2) (t_c3_3, p2, p1, p0, cin);
   or  #(2) (c3, g2, t_c3_1, t_c3_2, t_c3_3);
 
-  // c4 (cout) = g3 + p3*g2 + p3*p2*g1 + p3*p2*p1*g0 + p3*p2*p1*p0*cin
+ 
   and #(2) (t_c4_1, p3, g2);
   and #(2) (t_c4_2, p3, p2, g1);
   and #(2) (t_c4_3, p3, p2, p1, g0);
   and #(2) (t_c4_4, p3, p2, p1, p0, cin);
   or  #(2) (cout, g3, t_c4_1, t_c4_2, t_c4_3, t_c4_4);
 
-  //----------------------------------------------------------------------------
-  // Step 3: Sum bit generation
-  //----------------------------------------------------------------------------
+  
   xor #(2) (sum[0], p0, cin);
   xor #(2) (sum[1], p1, c1);
   xor #(2) (sum[2], p2, c2);
